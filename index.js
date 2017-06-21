241244ab5cdc1a8fa5fb1d4d42180081d5cf736a
Björn Hempel <bjoern.hempel@ressourcenmangel.de>
2017-06-21 13:48:07 +0200

    Improved detection algorithm; #2

--- index.js
+++ index.js
@@ -21,65 +21,164 @@ exports.builder.prototype.selfClosingTags = [
     'input',
     'keygen',
     'link',
+    'menuitem',
     'meta',
     'param',
+    'script',
     'source',
     'track',
     'wbr'
 ];
 
-/**
- * Finds unclosed tag and returns them (validation).
- *
- * @param {String} html The html string to validate.
- */
-exports.builder.prototype.getUnclosedTags = function(html) {
+exports.builder.prototype.getUnclosedTags = function(html, filename) {
 
     var unclosedTags = [];
 
     var lines = html.split('\n');
 
+    /* saves all tags that where found at the html variable */
+    var tags = [];
+
+    /* scan each line */
     for (var i = 0; i < lines.length; i++) {
-        var matchedTags = lines[i].match(/<(\/{1})?\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[^'">\s]+))?)+\s*|\s*)>/g);
 
-        if (matchedTags) {
-            for (var j = 0; j < matchedTags.length; j++) {
-                /* remove already closed tags from unclosedTags */
-                if (matchedTags[j].indexOf('</') >= 0) {
-                    var closeTagName = matchedTags[j].substr(2, matchedTags[j].length - 3);
-                    closeTagName = closeTagName.replace(/ /g, '');
+        /* find opening and closing html tags */
+        var matchedTags = lines[i].match(/<[^>]*[^/]>/g) || [];
 
-                    for (var k = unclosedTags.length - 1; k >= 0; k--) {
-                        if (unclosedTags[k].element === closeTagName) {
-                            /* remove tag */
-                            unclosedTags.splice(k, 1);
+        for (var j = 0; j < matchedTags.length; j++) {
 
-                            if (closeTagName !== 'html') {
-                                break;
-                            }
-                        }
-                    }
+            var matchedTag = matchedTags[j];
 
-                /* add open tags to unclosedTags */
-                } else {
-                    var unclosedTag = {};
+            var matches = matchedTag.match(/<\/?([a-z0-9]+)/i);
 
-                    unclosedTag.full = matchedTags[j];
-                    unclosedTag.line = i + 1;
-
-                    if (unclosedTag.full.indexOf(' ') > 0) {
-                        unclosedTag.element = unclosedTag.full.substr(1, unclosedTag.full.indexOf(' ') - 1);
-                    } else {
-                        unclosedTag.element = unclosedTag.full.substr(1, unclosedTag.full.length - 2);
-                    }
-
-                    if (this.isSelfClosing(unclosedTag.element) === false) {
-                        unclosedTags.push(unclosedTag);
-                    }
-                }
+            if (matches) {
+                tags.push({
+                    tag: matchedTag,
+                    name: matches[1],
+                    line: i + 1,
+                    closing: matchedTag[1] === '/'
+                });
             }
+        };
+    };
+
+    /* no html tags found */
+    if (tags.length == 0) {
+        return unclosedTags;
+    }
+
+    var openTags = [];
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+
+        /* we do have a closing tag element */
+        if (tag.closing) {
+            var closingTag = tag;
+
+            /* do not check this tag */
+            if (this.isSelfClosing(closingTag.name)) {
+                continue;
+            }
+
+            /* noOpenTagFound: no open tags found */
+            if (openTags.length == 0) {
+                unclosedTags.push({
+                    tag: closingTag.tag,
+                    name: closingTag.name,
+                    filename: filename ? filename : null,
+                    line: closingTag.line,
+                    hasNoCloseTag: false,
+                    hasNoOpenTag: true
+                });
+                continue;
+            }
+
+            var openTag = openTags[openTags.length - 1];
+
+            /* noOpenTagFound: close tag does not match with the last open tag */
+            if (closingTag.name !== openTag.name) {
+
+                var foundOpeningTag = false;
+
+                for (var j = openTags.length - 1; j >= 0; j--) {
+                    var currentOpenTag = openTags[j];
+
+                    /* we found the open tag that matches with the closing tag element */
+                    if (closingTag.name === currentOpenTag.name) {
+                        foundOpeningTag = true;
+                        openTags.pop();
+                        break;
+                    }
+
+                    /* until we havn't found the opening element, all following tags are specified as unclosed */
+                    unclosedTags.push({
+                        tag: currentOpenTag.tag,
+                        name: currentOpenTag.name,
+                        filename: filename ? filename : null,
+                        line: currentOpenTag.line,
+                        hasNoCloseTag: true,
+                        hasNoOpenTag: false
+                    });
+
+                    openTags.pop();
+                }
+
+                if (!foundOpeningTag) {
+                    unclosedTags.push({
+                        tag: closingTag.tag,
+                        name: closingTag.name,
+                        filename: filename ? filename : null,
+                        line: closingTag.line,
+                        hasNoCloseTag: false,
+                        hasNoOpenTag: true
+                    });
+                }
+
+                continue;
+
+            /* openTagFound: remove last open tag */
+            } else {
+                openTags.pop();
+            }
+
+        /* we do have an opening tag element */
+        } else {
+
+            var openingTag = tag;
+
+            /* do not check this tag */
+            if (this.isSelfClosing(openingTag.name)) {
+                continue;
+            }
+
+            /* add opening tag to our openTags list */
+            openTags.push(openingTag);
         }
     }
+
+    /* we do have some open tags over */
+    if (openTags.length > 0) {
+        for (var i = 0; i < openTags.length; i++) {
+            var tag = openTags[i];
+
+            unclosedTags.push({
+                tag: tag.tag,
+                name: tag.name,
+                filename: filename ? filename : null,
+                line: tag.line,
+                hasNoCloseTag: true,
+                hasNoOpenTag: false
+            });
+        }
+    }
+
+    /* sort unclosedTags list */
+    unclosedTags.sort(
+        function(a, b) {
+            return (a.line > b.line) ? 1 : ((b.line > a.line) ? -1 : 0);
+        }
+    );
 
     return unclosedTags;
 };
@@ -107,16 +206,24 @@ exports.builder.prototype.isSelfClosing = function(tagName) {
 exports.builder.prototype.printUnclosedTags = function(unclosedTags) {
 
     if (unclosedTags.length == 0) {
-        console.info('Congratulations! No unclosed tags.');
-    } else {
-        if (unclosedTags.length == 1) {
-            console.info('The following tag doesn\'t seem to be closed');
-        } else {
-            console.info('The following tags don\'t seem to be closed');
-        }
+        return;
+    }
 
-        for (var i = 0; i < unclosedTags.length; i++) {
-            console.info('line ' + unclosedTags[i].line + ': ' + unclosedTags[i].full);
+    /* prints out missing close tag issues */
+    for (var i = 0; i < unclosedTags.length; i++) {
+        if (unclosedTags[i].hasNoCloseTag) {
+            console.info(unclosedTags[i].filename + ':' + unclosedTags[i].line + ' (missing close tag: <' + unclosedTags[i].name + '/>)');
+            console.info(unclosedTags[i].tag);
+            console.info('');
+        }
+    }
+
+    /* prints out missing open tag issues */
+    for (var i = 0; i < unclosedTags.length; i++) {
+        if (unclosedTags[i].hasNoOpenTag) {
+            console.info(unclosedTags[i].filename + ':' + unclosedTags[i].line + ' (missing open tag: <' + unclosedTags[i].name + '>)');
+            console.info(unclosedTags[i].tag);
+            console.info('');
         }
     }
 };
